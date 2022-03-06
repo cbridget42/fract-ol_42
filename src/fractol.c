7345e7dd724abe464990ebb215e192809b9796d0
Coach Bridget <cbridget@il-f1.msk.21-school.ru>
2022-03-06 17:43:13 +0300

    tests

--- src/fractol.c
+++ src/fractol.c
@@ -3,14 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbridget <cbridget@student-21school.ru>    +#+  +:+       +#+        */
+/*   By: cbridget <cbridget@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 12:43:08 by cbridget          #+#    #+#             */
-/*   Updated: 2022/03/05 16:30:29 by cbridget         ###   ########.fr       */
+/*   Updated: 2022/03/06 17:31:41 by cbridget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
+#include <mlx.h>
+
+
+#include <stdio.h>
 
 typedef struct	s_data
 {
@@ -22,6 +25,7 @@ typedef struct	s_data
 }				t_data;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+void put_square(int size, t_data *img);
 
 int	main(void)
 {
@@ -30,19 +34,67 @@ int	main(void)
 	t_data	img;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 800, 600, "Hello world!");
-	img.img = mlx_new_image(mlx, 800, 600);
+	mlx_win = mlx_new_window(mlx, 1000, 1000, "Hello world!");
+	img.img = mlx_new_image(mlx, 1000, 1000);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lenght, &img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+//	my_mlx_pixel_put(&img, 500, 500, 0x00FF0000);
+	put_square(200, &img);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return 0;
 }
 
+void put_square(int size, t_data *img)
+{
+	int x = 100;
+	int y = 100;
+	int i, j;
+	i = 0;
+	while (i < 1000)
+	{
+		j = 0;
+		int r = 255;
+		int g = 0;
+		int b = 0;
+		while (j < 1000)
+		{
+			if (r == 255 && b < 255)
+				b++;
+			else if (r > 0 && b == 255)
+				r--;
+			else if (b == 255 && g < 255)
+				g++;
+			else
+				b--;
+			int tmp = (r<<16) | (g<<8) | b;
+//			printf("%x\n", tmp);
+			my_mlx_pixel_put(img, j, i, tmp);
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+//	printf("%x\n", 0xFF000000);
+	while (i < size)
+	{
+		int r = 255;
+		int g = 255;
+		int b = 255;
+		j = 0;
+		while (j < size)
+		{
+			int tmp = (r<<16) | (g<<8) | b;
+			my_mlx_pixel_put(img, x + j, y + i, tmp);
+			r--;
+			g--;
+			b--;
+			j++;
+		}
+		i++;
+	}
+}
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	char	*dst;
-
-	dst = data->addr + (y * data->line_lenght + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*((int *)(data->addr) + y * data->line_lenght / (data->bits_per_pixel / 8) + x) = color;
 }
